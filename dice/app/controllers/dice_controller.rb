89dc0d777aea2e25ad1a1b2eb564d4668f8d5fb7
dmainka <dmainka@gmail.com>
2014-01-26 17:24:57 -0600

    Changed buttons to button_to; added some comments

--- dice/app/controllers/dice_controller.rb
+++ dice/app/controllers/dice_controller.rb
@@ -1,60 +1,74 @@
 class DiceController < ApplicationController
 
+  # handle the home screen (gives instructions and a button to start the games)
   def home
-    render "home"
+    render "home_view"
   end
 
+  # handle the first roll which checks that the player doesn't immediately
+  # win or lose.  If neither applies then we set the goal equal to the sum
+  # of the dice which is passed as a query string parameter
   def start
+    # simulate rolling the dice
     dice = [rand(1..6), rand(1..6)]
     sum = dice[0] + dice[1]
 
+    # present images corresponding to the simulated dice rolls
     @img1 =  "https://uchicago.s3.amazonaws.com/d0#{dice[0].to_s}.png"
     @img2 =  "https://uchicago.s3.amazonaws.com/d0#{dice[1].to_s}.png"
 
+    # game logic
     if (sum == 7) || (sum == 11)
+        @goal = nil
         @msg1 = "YOU WIN!"
         @msg2 = nil
-        @button_link = "'/dice'"
+        @button_path = dice_path
         @button_text = "Start over"
     elsif (sum == 2) || (sum == 3) || (sum == 12)
+        @goal = nil
         @msg1 = "YOU LOSE."
         @msg2 = nil
-        @button_link = "'/dice'"
+        @button_path = dice_path
         @button_text = "Start over"
     else
         @goal = sum
         @msg1 = "Your goal is now #{@goal.to_s}."
-        @msg2 = "Now try to get a #{@goal.to_s} again before you roll a 7."
-        @button_link = "'/roll_again?Goal=#{@goal.to_s}'"
+        @msg2 = "Now try to get #{@goal.to_s} again before you roll a 7."
+        @button_path = roll_again_path("Goal" => @goal)
         @button_text = "Roll the dice"
     end
 
     render "roll_view"
   end
 
+  # handle all rolls after the first
   def roll_again
+    # get the goal from query string parameter
     @goal = params["Goal"].to_i
 
+    # simulate rolling the dice
     dice = [rand(1..6), rand(1..6)]
     sum = dice[0] + dice[1]
 
+    # present images corresponding to the simulated dice rolls
     @img1 =  "https://uchicago.s3.amazonaws.com/d0#{dice[0].to_s}.png"
     @img2 =  "https://uchicago.s3.amazonaws.com/d0#{dice[1].to_s}.png"
 
+    # game logic
     if (sum == 7)
         @msg1 = "YOU LOSE."
         @msg2 = nil
-        @button_link = "'/dice'"
+        @button_path = dice_path
         @button_text = "Start over"
     elsif (sum == @goal)
         @msg1 = "YOU WIN!"
         @msg2 = nil
-        @button_link = "'/dice'"
+        @button_path = dice_path
         @button_text = "Start over"
     else
-        @msg1 = "Your goal is #{@goal.to_s}."
-        @msg2 = "Try to get a #{@goal.to_s} again before you roll a 7."
-        @button_link = "'/roll_again?Goal=#{@goal.to_s}'"
+        @msg1 = "You rolled a #{sum.to_s}."
+        @msg2 = "Try to get another #{@goal.to_s} before you roll a 7."
+        @button_path = roll_again_path("Goal" => @goal)
         @button_text = "Roll the dice"
     end
 
--- dice/config/routes.rb
+++ dice/config/routes.rb
@@ -1,9 +1,16 @@
 Dice::Application.routes.draw do
 
-  get("/dice", { :controller => "dice", :action => "home"})
-  get("/start", { :controller => "dice", :action => "start"})
-  get("/roll_again", { :controller => "dice", :action => "roll_again"})
+  match '/dice', {:controller => "dice", :action => "home", :via => 'get'}
 
-  # match '/start', {to: 'dice#start', via: 'get', action: 'start'}
+  # due to the use of button_to buttons naturally POST (which can be overridden but I chose not to do that)
+  # so the routes below respond to the POST action
+  match '/dice', {:controller => "dice", :action => "home", :via => 'post'}
+  match '/start', {:controller => "dice", :action => "start", :via => 'post'}
+  match '/roll_again', {:controller => "dice", :action => "roll_again", :via => 'post'}
+
+  # this is old code based on in class example
+  # get("/dice", { :controller => "dice", :action => "home"})
+  # get("/start", { :controller => "dice", :action => "start"})
+  # get("/roll_again", { :controller => "dice", :action => "roll_again"})
 
 end
